7737fa1a01951349ab943b77413c0a4495bb611e
Lucas Venezian Povoa <lucasvenez@gmail.com>
2022-09-19 10:38:22 -0300

    Add single mapping code

--- mapping_single.py
+++ mapping_single.py
@@ -1,7 +1,7 @@
-from sqlalchemy import create_engine, Column, Integer
+from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base
 
-URL = "mysql+mysqlconnector://aluno:aluno123@localhost/ORM"
+URL = "mysql+mysqlconnector://aluno:aluno123@localhost:3306/ORM"
 
 # $ cd C:\Program Files\MySQL\MySQL Server 8.0\bin
 # $ .\mysql.exe -u aluno -p
@@ -14,13 +14,13 @@ Base = declarative_base()
 class Pessoa(Base):
     __tablename__ = "Pessoa"
     id_pessoa = Column(Integer, primary_key=True)
+    nome = Column(String(150), nullable=False)
 
 
 def main():
     engine = create_engine(url=URL)
-
-    with engine.connect() as connection:
-        pass
+    Base.metadata.drop_all(bind=engine)
+    Base.metadata.create_all(bind=engine)
 
 
 if __name__ == "__main__":
