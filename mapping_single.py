f0a44233a6a5f839cc0148b8aa2b56e177fbbd78
Lucas Venezian Povoa <lucasvenez@gmail.com>
2022-09-19 11:20:14 -0300

    Add single mapping code

--- mapping_single.py
+++ mapping_single.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 URL = "mysql+mysqlconnector://aluno:aluno123@localhost:3306/ORM"
 
@@ -19,8 +19,22 @@ class Pessoa(Base):
 
 def main():
     engine = create_engine(url=URL)
-    Base.metadata.drop_all(bind=engine)
+    #Base.metadata.drop_all(bind=engine)
     Base.metadata.create_all(bind=engine)
+    # mysql> DESC Pessoa;
+
+    Session = sessionmaker(engine, expire_on_commit=False)
+
+    with Session.begin() as session:
+        pessoa = Pessoa(nome="Jhon Snow")
+        id_pessoa = pessoa.id_pessoa
+        session.add(pessoa)
+
+    with Session.begin() as session:
+        pessoa.nome = "Jhon Snow, The King Forever"
+        id_pessoa = pessoa.id_pessoa
+        session.add(pessoa)
+
 
 
 if __name__ == "__main__":
